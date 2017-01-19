4b648f4ad02873b095023c9e0926d7a6c3cbba30
Diego <die.stefanelli@gmail.com>
2017-01-18 22:11:52 -0300

    upload popup table

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
 	var $header = $(".site-header")
 		, $menu = $(".menu")
 		, $nav = $(".site-nav")
@@ -24,4 +24,63 @@ $(document).ready(function () {
 	var download = function (file) {
 		document.location = file;
 	}
+});*/
+var MATHS = MATHS || {};
+
+/*
+	POPUP
+*/
+MATHS.Popup = {};
+MATHS.Popup.open = function( id, callback ){
+	$( "#"+id ).addClass( "active" );
+	$('body').addClass("hidden");
+	if( callback ){
+		callback();
+	}
+}
+
+MATHS.Popup.close = function( id, callback ){
+	$( "#"+id ).removeClass( "active" );
+	$('body').removeClass("hidden");
+	if( callback ){
+		callback();
+	}
+}
+
+MATHS.Modules = function( id ){
+	
+}
+
+MATHS.download =  function( file ){
+	document.location = file;
+}
+
+MATHS.init = function(){
+	
+	var $header = $(".site-header")
+		, $menu = $(".menu")
+		, $nav = $(".site-nav")
+		, $tool = $(".cv-tool");
+	
+	$menu.on("click", function () {
+		$(this).toggleClass("active");
+		$(this).parent().find($nav).toggleClass("active");
+	});
+	
+	$tool.on("click", function(){
+		$(this).siblings(".tool-info").toggleClass("active");
+	});
+	
+	var $grid = $('.grid').imagesLoaded(function () {
+		$grid.masonry({
+			columnWidth: '.grid-sizer'
+			, itemSelector: '.grid-item'
+			, percentPosition: true
+		});
+	});
+}
+
+$(function(){
+	MATHS.init();
+
 });
